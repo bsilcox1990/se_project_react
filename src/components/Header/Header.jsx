25fc6ab074498d52a2aacc7ea23a0c4c819b19c6
Bradley Silcox <bradleysilcox80@gmail.com>
2025-02-25 11:55:45 -0800

    add basic file structure, add normalize.css, add required font styles, add images for header and favicon

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p className="header__text">This is some text</p>
+    </>
+  );
+}
+
+export default Header;
