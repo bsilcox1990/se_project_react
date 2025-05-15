5b622bb72ebcb1388c577261f629829cef4ebc99
Bradley Silcox <bradleysilcox80@gmail.com>
2025-05-15 14:19:20 -0700

    refactor: update footer date to get current year dynamically

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__author">Devolped by Bradley Silcox</p>
-      <p className="footer__year">2025</p>
+      <p className="footer__year">{new Date().getFullYear()}</p>
     </footer>
   );
 }
