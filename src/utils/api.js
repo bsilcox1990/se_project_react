33b650f6c166e053cb423a39c77c9034e052404e
Bradley Silcox <bradleysilcox80@gmail.com>
2025-06-03 12:21:30 -0700

    fix: change basename in vite config to fix error in deploying frontend to google cloud server, chore: update homepage to match domain name, update deploy script to copy frontend onto google server and not gh pages, modify baseurl for api to work for production and development

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-export const baseUrl = "http://localhost:3001";
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwr-tripleten.jumpingcrab.com"
+    : "http://localhost:3001";
 const headers = { "Content-Type": "application/json" };
 
 export function sendRequest({ url, options }) {
